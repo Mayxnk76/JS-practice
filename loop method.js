ed3a98d7d6de468b55aaeb18483a8833ee35beae
Mayank <mayankrahani@gmail.com>
2026-07-26 19:00:37 +0530

    add loop

---
+++ loop method.js
@@ -0,0 +1,27 @@
+//  1 print num,bers 1 to 10
+
+for (let i = 1; i <= 10; i++){
+  console.log(i);
+}
+
+// // 2 print number 10 to 2
+
+// for(let i = 10; i >= 1; i--){
+//   console.log(i);
+// }
+
+// // 3 print even number 1 to 20
+
+// for(let i = 1; i <= 20; i++){
+//   if(i % 2 === 0){
+//     console.log(i);
+//   }
+// }
+
+// // 4 print odd number 1 to 20
+
+// for(let i = 1; i <= 20; i++){
+//   if(i % 2 === 1){
+//     console.log(i);
+//   }
+// }
