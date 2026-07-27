d908e5cf777bdebf74fcedfb0ab1cdadfce373c4
Mayank76x <mayankrahani@email.com>
2026-07-27 15:58:41 +0530

    Add JavaScript loop practice exercises

---
+++ loop method.js
@@ -0,0 +1,35 @@
+//  1 print num,bers 1 to 10
+
+for (let i = 1; i <= 10; i++){
+  console.log(i);
+}
+
+// 2 print number 10 to 2
+
+for(let i = 10; i >= 1; i--){
+  console.log(i);
+}
+
+// 3 print even number 1 to 20
+
+for(let i = 1; i <= 20; i++){
+  if(i % 2 === 0){
+    console.log(i);
+  }
+}
+
+// 4 print odd number 1 to 20
+
+for(let i = 1; i <= 20; i++){
+  if(i % 2 === 1){
+    console.log(i);
+  }
+}
+
+// 5 find the sum of number 1 to 100 
+
+let n = 0;
+for(let i = 1; i <= 100; i++){
+   n = n + i;
+}
+console.log(n);
